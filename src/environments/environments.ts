a8124bbf05a47ced9c9bd118f539ea27d2575180
Matheus Honorato <158205261+matheusHsousa@users.noreply.github.com>
2026-03-04 09:35:33 -0300

    remoção barra a mais

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -10,5 +10,5 @@ export const environment = {
       measurementId: "G-7L4EKV9FSY"
     }
   ,
-  apiBase: 'https://backend-sistema-clube-production.up.railway.app/'
+  apiBase: 'https://backend-sistema-clube-production.up.railway.app'
 }
